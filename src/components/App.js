eebc7e512f290679ef1e01c22f783cb2982fd84b
alcocere <elisa.alcocerm@gmail.com>
2021-02-24 08:49:13 +0100

    Add first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import data from '../data/data.json';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Hola Mundo
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/App.scss';
+import './stylesheets/index.scss';
+import App from './components/App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
